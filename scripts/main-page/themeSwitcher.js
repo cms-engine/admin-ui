6e5ef5132c23bbd89d233dfbc7eb2eca332d438f
holus-bolus <ustinovoleksij@gmail.com>
2024-12-16 11:04:08 +0200

    fixed bugs;

--- scripts/main-page/themeSwitcher.js
+++ scripts/main-page/themeSwitcher.js
@@ -4,23 +4,32 @@
  * and class styling based on the current theme.
  */
 
-export const initializeThemeSwitcher = () => {
-    const themeSwitcher = document.getElementById("themeSwitcher");
-    themeSwitcher.addEventListener("click", () => {
-        const body = document.body;
-        body.classList.toggle("light-theme");
-        body.classList.toggle("dark-theme");
+const initializeThemeSwitcher = () => {
+    const themeSwitcher = document.getElementById('themeSwitcher')
+    const body = document.body
+    const savedTheme = localStorage.getItem('theme') || 'light'
+    const themeIcon = themeSwitcher.querySelector('i')
 
-        const icon = themeSwitcher.querySelector("i");
+    if (savedTheme === 'dark') {
+        body.classList.add('dark-theme')
+        themeIcon.classList.replace('bi-moon', 'bi-sun')
+    } else {
+        body.classList.remove('dark-theme')
+        themeIcon.classList.replace('bi-sun', 'bi-moon')
+    }
 
-        if (body.classList.contains("light-theme")) {
-            icon.classList.remove("bi-moon");
-            icon.classList.add("bi-sun");
-            themeSwitcher.classList.replace("btn-outline-light", "btn-outline-dark");
+    themeSwitcher.addEventListener('click', () => {
+        const isDarkTheme = body.classList.toggle('dark-theme')
+        console.log(body.className)
+        if (isDarkTheme) {
+            themeIcon.classList.replace('bi-moon', 'bi-sun')
+            localStorage.setItem('theme', 'dark')
         } else {
-            icon.classList.remove("bi-sun");
-            icon.classList.add("bi-moon");
-            themeSwitcher.classList.replace("btn-outline-dark", "btn-outline-light");
+            themeIcon.classList.replace('bi-sun', 'bi-moon')
+            localStorage.setItem('theme', 'light')
         }
-    });
-};
+    })
+}
+
+
+initializeThemeSwitcher()
