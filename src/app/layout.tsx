ad19d99e28387213e3c8f031c16316b79432aa3d
holus-bolus <ustinovoleksij@gmail.com>
2024-12-29 13:17:52 +0200

    Added the header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@
 import './globals.css'
 import { Provider } from 'react-redux'
 import { store } from '@/store'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 export default function RootLayout({
   children,
 }: Readonly<{
--- src/components/src/components/Layout/Header.tsx
+++ src/components/src/components/Layout/Header.tsx
@@ -1,39 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
+import styles from './Header.module.css'
 
 const Header: React.FC = () => {
+  const [currentTime, setCurrentTime] = useState<string>('')
+  const [dropdownIsVisible, setDropdownIsVisible] = useState(false)
+
+  const toggleDropdown = () => setDropdownIsVisible(!dropdownIsVisible)
+
+  useEffect(() => {
+    const updateClock = () => {
+      const now = new Date()
+      const time = now.toLocaleTimeString('en-US', { hour12: false })
+      setCurrentTime(time)
+    }
+
+    updateClock()
+    const interval = setInterval(updateClock, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
   return (
     <nav className='navbar'>
-      <div className='navbar-left'>
-        <Image
-          src={'/public/logo.png'}
-          alt={'The website logo'}
-          width={40}
-          height={40}
-        />
-      </div>
-      <form className='search-bar'>
-        <label>
-          <input type='search' placeholder='Search...' />
-        </label>
-        <button type='submit'>
-          <i className='bi bi-search'></i>
-        </button>
-      </form>
-      <div className='navbar-right'>
-        <button id='themeSwitcher' className='btn btn-outline-light btn-sm'>
-          <i className='bi bi-moon'></i>
-        </button>
-        <div className='dropdown'>
-          <button className='btn dropdown-toggle'>John Doe</button>
-          <ul className='dropdown-menu'>
-            <li>
-              <a href='#'>Personal Settings</a>
-            </li>
-            <li>
-              <a href='#'>Logout</a>
-            </li>
-          </ul>
+      <div className={`container ${styles.headerContainer}`}>
+        <div className={`navbar-left ${styles.searchContainer}`}>
+          <Image
+            src={'/logo.png'}
+            alt={'The website logo'}
+            width={40}
+            height={40}
+          />
+          <form className={styles.searchForm}>
+            <label className={styles.searchLabel}>
+              <i className={`bi bi-search ${styles.searchIcon}`}></i>
+              <input
+                type='search'
+                placeholder='Search'
+                className={styles.searchInput}
+              />
+            </label>
+          </form>
+        </div>
+        <div className={`navbar-right ${styles.clockContainer}`}>
+          <span className='current-time'>{currentTime}</span>
+          <button id='themeSwitcher' className='btn btn-outline-dark btn-sm'>
+            <i className='bi bi-moon'></i>
+          </button>
+          <div className='dropdown'>
+            <button
+              className={`btn ${styles.dropdownToggle}`}
+              onClick={toggleDropdown}
+            >
+              John Doe
+            </button>
+            <ul
+              className={`${styles.dropdownMenu}
+               ${dropdownIsVisible ? styles.showDropdown : ''}`}
+            >
+              <li>
+                <a href='#'>Personal Settings</a>
+              </li>
+              <li>
+                <a href='#'>Logout</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
