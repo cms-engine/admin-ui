bc00456b2346a63a70039fd4dfb247a8ceadf2b6
holus-bolus <ustinovoleksij@gmail.com>
2025-01-13 11:03:48 +0200

    Added the import

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(req) {
-  // Example middleware logic
   const url = req.nextUrl.clone()
   console.log(`Middleware triggered for ${url.pathname}`)
   return NextResponse.next()
